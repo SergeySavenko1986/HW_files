9624bfc3d6113d263bc3ee772f156cd6762b2c1b
sesavi test <sesavi@mail.ru>
2023-03-22 00:59:08 +0300

    task_3 update

--- Task_3.py
+++ Task_3.py
@@ -1,21 +1,151 @@
 from pprint import pprint
-
-with open('1.txt', 'rt', encoding='utf-8') as file1:
+file1 = {}
+# file2 = {}
+# file3 = {}
+with open('1.txt', 'rt', encoding='utf-8') as file:
     a1 = 0
-    for line1 in file1:
-        print(line1)
+    i = 0
+
+    for line in file:
+        print(line)
         a1 += 1
+        i += 1
+        file1[i] = line
 
-with open('2.txt', 'rt', encoding='utf-8') as file2:
+with open('2.txt', 'rt', encoding='utf-8') as file:
     a2 = 0
-    for line2 in file2:
-        print(line2)
+    for line in file:
+        print(line)
         a2 += 1
+        i += 1
+        file1[i] = line
 
-with open('3.txt', 'rt', encoding='utf-8') as file3:
+with open('3.txt', 'rt', encoding='utf-8') as file:
     a3 = 0
-    for line3 in file3:
-        print(line3)
+    for line in file:
+        print(line)
         a3 += 1
+        i += 1
+        file1[i] = line
 
-dict (a1, a2, a3)
+print (a1, a2, a3)
+
+with open('Task_3.txt', 'wt') as f:
+
+    if a2 < a1 < a3:
+        f.write(f'2.txt\n{a2}\n')
+        i = a1 + 1
+        while i <= a1 + a2:
+            f.write(file1[i])
+            i += 1
+
+        f.write(f'\n1.txt\n{a1}\n')
+        i = 1
+        while i <= a1:
+            f.write(file1[i])
+            i += 1
+
+        f.write(f'\n3.txt\n{a3}\n')
+        i = a1 + a2 + 1
+        while i <= a1 + a2 + a3:
+            f.write(file1[i])
+            i += 1
+
+    if a2 < a3 < a1:
+        f.write(f'2.txt\n{a2}\n')
+        i = a1 + 1
+        while i <= a1 + a2:
+            f.write(file1[i])
+            i += 1
+
+        f.write(f'\n3.txt\n{a3}\n')
+        i = a1 + a2 + 1
+        while i <= a1 + a2 + a3:
+            f.write(file1[i])
+            i += 1
+
+        f.write(f'\n1.txt\n{a1}\n')
+        i = 1
+        while i <= a1:
+            f.write(file1[i])
+            i += 1
+
+    if a1 < a2 < a3:
+        f.write(f'1.txt\n{a1}\n')
+        i = 1
+        while i <= a1:
+            f.write(file1[i])
+            i += 1
+
+        f.write(f'\n2.txt\n{a2}\n')
+        i = a1 + 1
+        while i <= a1 + a2:
+            f.write(file1[i])
+            i += 1
+
+        f.write(f'\n3.txt\n{a3}\n')
+        i = a1 + a2 + 1
+        while i <= a1 + a2 + a3:
+            f.write(file1[i])
+            i += 1
+
+
+    if a1 < a3 < a2:
+        f.write(f'1.txt\n{a1}\n')
+        i = 1
+        while i <= a1:
+            f.write(file1[i])
+            i += 1
+
+        f.write(f'\n3.txt\n{a3}\n')
+        i = a1 + a2 + 1
+        while i <= a1 + a2 + a3:
+            f.write(file1[i])
+            i += 1
+
+        f.write(f'\n2.txt\n{a2}\n')
+        i = a1 + 1
+        while i <= a1 + a2:
+            f.write(file1[i])
+            i += 1
+
+
+    if a3 < a1 < a2:
+        f.write(f'3.txt\n{a3}\n')
+        i = a1 + a2 + 1
+        while i <= a1 + a2 + a3:
+            f.write(file1[i])
+            i += 1
+
+
+        f.write(f'\n1.txt\n{a1}\n')
+        i = 1
+        while i <= a1:
+            f.write(file1[i])
+            i += 1
+
+        f.write(f'\n2.txt\n{a2}\n')
+        i = a1 + 1
+        while i <= a1 + a2:
+            f.write(file1[i])
+            i += 1
+
+
+    if a3 < a2 < a1:
+        f.write(f'3.txt\n{a3}\n')
+        i = a1 + a2 + 1
+        while i <= a1 + a2 + a3:
+            f.write(file1[i])
+            i += 1
+
+        f.write(f'\n2.txt\n{a2}\n')
+        i = a1 + 1
+        while i <= a1 + a2:
+            f.write(file1[i])
+            i += 1
+
+        f.write(f'\n1.txt\n{a1}\n')
+        i = 1
+        while i <= a1:
+            f.write(file1[i])
+            i += 1
